91c80088f1e5a2d60572283f7d937f794be46caf
Ankit Bahl <ankit2bahl@gmail.com>
2018-05-19 21:03:06 -0400

    added boilerplate and a basic login page

---
+++ src/Login.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './Login.css'
+import login from './LoginManager'
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      key: ''
+    }
+  }
+
+  onInput = (e) => {
+    switch(e.target.id) {
+      case '0':
+        this.setState({username: e.target.value})
+        break;
+      case '1':
+        this.setState({password: e.target.value})
+        break;
+      default:
+        break;
+    }
+  };
+
+  onKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      this.onSubmit();
+    }
+  };
+
+  onSubmit = () => {
+    login(this.state.username, this.state.password, (resp) => {this.setState({key: resp.key})});
+  };
+  render() {
+    return (
+      <div className="Login">
+        <header>
+          Welcome to KuraFS
+        </header>
+        <p>
+          Please log in to continue:
+        </p>
+        <div>
+          <div>
+            username: <input id="0" onChange={this.onInput}/>
+          </div>
+          <div className="pass">
+            password: <input type="password" id="1" onChange={this.onInput} onKeyDown={this.onKeyPress}/>
+          </div>
+          <button onClick={this.onSubmit}>Login</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './Login';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Login />, document.getElementById('root'));
+registerServiceWorker();
